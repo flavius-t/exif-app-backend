8234d2bfa8e27e44b0eac727a3d3a1adf8cffdc8
flavius-t <flavius.t@hotmail.com>
2023-06-12 13:54:06 -0700

    enable CORS for frontend, upload multiple images

--- exif.py
+++ exif.py
@@ -1,17 +1,22 @@
 from flask import Flask, request
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/upload', methods=['POST'])
 def handle_upload():
-    if 'image' not in request.files:
-        return 'No file uploaded', 400
+    if not request.files:
+        return 'No files uploaded', 400
 
-    image = request.files['image']
+    for key, file in request.files.items():
+        # TODO: process files, check file type, etc.
+        print(key, file)
 
-    return 'Image uploaded successfully'
+    # TODO: respond with processed files
+    return 'Images uploaded successfully', 200
 
 
 if __name__ == '__main__':
