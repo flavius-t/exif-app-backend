b29c653c85a649dec88f75638735db3111619884
flavius-t <flavius.t@hotmail.com>
2023-06-14 14:06:43 -0700

    unzip images from frontend

--- exif.py
+++ exif.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
 from flask_cors import CORS
+import os
+from utils.extract_meta import extract_metadata, remove_exif
+from utils.zip import unzip_file
+
 
 
 app = Flask(__name__)
@@ -10,13 +14,26 @@ CORS(app)
 def handle_upload():
     if not request.files:
         return 'No files uploaded', 400
+    
+    if 'file' not in request.files:
+        return 'Request is missing zipfile', 400
+    
+    # TODO: modularize all of this
+    file = request.files['file']
+    upload_folder = 'temp'
+    os.makedirs(upload_folder, exist_ok=True)
+    zip_path = os.path.join(upload_folder, 'images.zip')
+    file.save(zip_path)
 
-    for key, file in request.files.items():
-        # TODO: process files, check file type, etc.
-        print(key, file)
+    imgs_folder = 'temp/images'
+    os.makedirs('temp/images', exist_ok=True)
+    unzip_file(zip_path, imgs_folder)
+
+    response = {}
+    print(response)
 
     # TODO: respond with processed files
-    return 'Images uploaded successfully', 200
+    return response, 200
 
 
 if __name__ == '__main__':
---
+++ utils/zip.py
@@ -0,0 +1,5 @@
+import zipfile
+
+def unzip_file(zip_path, extract_dir):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_dir)
