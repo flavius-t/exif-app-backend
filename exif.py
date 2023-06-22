c97785e9afd5234a3c9b6dee6a2168f0ef0823d2
flavius-t <flavius.t@hotmail.com>
2023-06-22 13:53:31 -0700

    refactor temp file saving, add temp file removing

--- exif.py
+++ exif.py
@@ -1,7 +1,9 @@
+import io
 import os
 import shutil
+import uuid
 
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, after_this_request
 from flask_cors import CORS
 from utils.extract_meta import extract_metadata
 from utils.zip import unzip_file, zip_files
@@ -23,20 +25,27 @@ def handle_upload():
     file = request.files['file']
 
     # save images to temp folder
-    os.makedirs(IMAGES_FOLDER, exist_ok=True)
-    zip_path = os.path.join(UPLOAD_FOLDER, ZIP_NAME)
+    base_folder = f'{UPLOAD_FOLDER}/{str(uuid.uuid4())}'
+    imgs_folder = f'{base_folder}/images'
+    os.makedirs(imgs_folder) # TODO: handle already exists error
+    zip_path = os.path.join(base_folder, ZIP_NAME)
     file.save(zip_path)
-    unzip_file(zip_path, IMAGES_FOLDER)
+    file.close()
+    unzip_file(zip_path, imgs_folder)
 
-    extract_metadata(IMAGES_FOLDER)
+    extract_metadata(imgs_folder)
 
-    zip_files(zip_path, IMAGES_FOLDER)
+    zip_files(zip_path, imgs_folder)
 
-    response = send_file(zip_path, as_attachment=True)
+    # TODO: error handling
+    return_data = io.BytesIO()
+    with open(zip_path, 'rb') as fo:
+        return_data.write(fo.read())
+    return_data.seek(0)
 
-    shutil.rmtree(IMAGES_FOLDER)
+    shutil.rmtree(base_folder)
 
-    return response, 200
+    return send_file(return_data, as_attachment=True, mimetype="application/zip", download_name="images.zip"), 200
 
 
 if __name__ == '__main__':
