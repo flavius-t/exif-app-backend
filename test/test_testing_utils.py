4a1e61f37800076c1691f11d7ba7d75dfb78b8f3
flavius-t <flavius.t@hotmail.com>
2023-07-22 14:33:00 -0700

    documentation

--- test/test_testing_utils.py
+++ test/test_testing_utils.py
@@ -1,9 +1,13 @@
+"""
+Unit tests for the testing_utils module.
+"""
+
 import pytest
 
 from test.testing_utils import create_file_of_size
 
 @pytest.mark.parametrize("file_size", [(1), (2), (10), (56), (99), (100)])
-def test_create_file_of_size(file_size):
+def test_create_file_of_size(file_size: int):
     """
     Tests that create_file_of_size correctly creates a file of the correct size.
     """
