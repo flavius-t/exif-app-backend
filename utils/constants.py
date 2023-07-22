d6fd6309ac85ae20005093d1b9a5efbc8ab6b7c7
flavius-t <flavius.t@hotmail.com>
2023-07-22 14:13:51 -0700

    update docs

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,12 @@
-import os
+"""
+Constants used by the application.
+"""
 
+# path to folder containing temporary folders for image processing
 UPLOAD_FOLDER = "temp"
-IMAGES_FOLDER = os.path.join(UPLOAD_FOLDER, "images")
+
+# name of zip file returned by /upload endpoint
 ZIP_NAME = "images.zip"
+
+# file extensions accepted by /upload endpoint
+ALLOWED_EXTENSIONS = set(["jpg", "jpeg", "png"])
